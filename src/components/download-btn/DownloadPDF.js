69b6e81eb19b1ea7b5be9883dc0287eee24621a5
Ahmed Anwar <aanwar@sumerge.com>
2021-07-18 01:27:38 +0200

    Add buttons to download with different quality

--- src/components/download-btn/DownloadPDF.js
+++ src/components/download-btn/DownloadPDF.js
@@ -1,20 +1,41 @@
 import {Component} from "react";
 import html2pdf from "html2pdf.js";
+import {Button, ButtonGroup, Dropdown} from "react-bootstrap";
 
 class DownloadPDF extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            quality: 4
+        }
     }
 
     render() {
-        return (<button onClick={this.downloadPdf}>Download as PDF</button>);
+        return (
+            // TODO: Styling :(
+            <Dropdown className="downloadPdf" as={ButtonGroup}>
+                <Button variant="info">Download PDF</Button>
+                <Dropdown.Toggle split variant="info"/>
+                <Dropdown.Menu>
+                    <Dropdown.Item onClick={() => {
+                        this.downloadPdf(4)
+                    }}>High Quality</Dropdown.Item>
+                    <Dropdown.Item onClick={() => {
+                        this.downloadPdf(2)
+                    }}>Medium Quality</Dropdown.Item>
+                    <Dropdown.Item onClick={() => {
+                        this.downloadPdf(1)
+                    }}>Low Quality</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        );
     }
 
-    downloadPdf() {
-        // TODO: Check if there is a way to print with better quality
+    downloadPdf(quality) {
         let opt = {
             filename: 'generated-resume.pdf',
-            jsPDF: {format: 'A4'}
+            jsPDF: {format: 'A4'},
+            html2canvas: {scale: quality}
         };
         html2pdf(document.getElementById("resume"), opt);
     }
